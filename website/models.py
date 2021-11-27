1485f8f39c40a7b2073a5272908f3b59b20e420d
Devin Lu <ludevinl@sfu.ca>
2021-11-27 15:05:38 -0800

    fixed models issue

--- website/models.py
+++ website/models.py
@@ -1,12 +1,15 @@
+from . import db
+from sqlalchemy.sql import func
+
 class User(db.Model):
-	phone_number = db.Column(db.CharField(max_length=10), primary_key=True)
-	first_name = db.column(db.String(26), nullable=False)
-	last_name = db.column(db.String(26), nullable=False)
-	email = db.column(db.String(320), nullable=False)
+	phone_number = db.Column(db.String(10), primary_key=True)
+	first_name = db.Column(db.String(26), nullable=False)
+	last_name = db.Column(db.String(26), nullable=False)
+	email = db.Column(db.String(320), nullable=False)
 
 
 class Listings(db.Model):
-	user = db.Column(db.CharField(max_length=10), ForeignKey("user.phone_number"), primary_key=True)
+	user = db.Column(db.String(10), db.ForeignKey("user.phone_number"), primary_key=True)
 	listing_id = db.Column(db.String(10), primary_key=True)
 	title = db.Column(db.String(128), nullable=False)
 	description = db.Column(db.String(512), nullable=False)
@@ -15,6 +18,6 @@ class Listings(db.Model):
 
 class Images(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	img_1 = db.Column(String(255))
-	img_2 = db.Column(String(255))
-	img_3 = db.Column(String(255))
+	img_1 = db.Column(db.String(255))
+	img_2 = db.Column(db.String(255))
+	img_3 = db.Column(db.String(255))
