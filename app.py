a272ee04102b1cf24780fa72857f4fb19723dd5e
Devin Lu <ludevinl@sfu.ca>
2021-11-27 14:14:58 -0800

    fixed

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
 from website import create_app
-from flask import Flask, request, redirect, render_template
 
-app = Flask(__name__)
-@app.route('/', methods =["GET", "POST"])
-def index():     
-  return render_template("base.html")
 app = create_app()
 
 if __name__ == '__main__':
--- website/views.py
+++ website/views.py
@@ -1,7 +1,8 @@
 from flask import Blueprint
+from flask import Flask, request, redirect, render_template
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
-def home():
-    return "<h1>testing</h1>"
+@views.route('/', methods =["GET", "POST"])
+def index():     
+  return render_template("base.html")
