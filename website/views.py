b5b9cc439ab7baac08651667fc259cdc5439cc56
Devin Lu <ludevinl@sfu.ca>
2021-11-27 17:04:17 -0800

    fixed indentation

--- website/views.py
+++ website/views.py
@@ -1,5 +1,7 @@
 from flask import Blueprint
 from flask import Flask, request, redirect, render_template
+from .models import User, Listings
+from . import db
 
 views = Blueprint('views', __name__)
 
@@ -11,29 +13,30 @@ def index():
 def showListings():
   return render_template("/listings.html")
 
-@views.route("/create_listing", methods =["POST"])
+@views.route("/create_listing", methods =["POST", "GET"])
 def postListings():
 
-  if request.method == "POST":
-  	email = request.form['input_email']
-  	phone = request.form['input_phone']
-  	fname = request.form['input_fname']
-  	lname = request.form['input_lname']
-  	title = request.form['input_title']
-  	location = request.form['input_location']
-  	description = request.form['input_desc']
-  	image = request.form['input_photo']
+    if request.method == "POST":
+        email = request.form['input_email']
+        phone = request.form['input_phone']
+        fname = request.form['input_fname']
+        lname = request.form['input_lname']
+        title = request.form['input_title']
+        location = request.form['input_location']
+        description = request.form['input_desc']
+        image = request.form['input_photo']
 
-  	poster = User(phone_number=phone, first_name=fname, last_name=lname, email=email)
-  	listing = Listings(phone, title, description, location)
+        poster = User(phone_number=phone, first_name=fname, last_name=lname, email=email)
+        listing = Listings(phone, title, description, location)
 
+        user_exists = db.session.query(User.phone_number).filter_by(phone_number=poster.phone_number).first() is not None
 
-    with app.app_context():
-        exists = db.session.query(User.phone_number).filter_by(phone_number=entries.phone_number).first() is not None
-        if not exists:
-            db.session.add(entries)
+        if not user_exists:
+            db.session.add(poster)
             db.session.commit()
 
+    if request.method == "GET":
+        return render_template("/create_listing.html")
 
 
 
