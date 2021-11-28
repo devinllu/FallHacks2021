d1d149f0146beb3483f1b7f78028706362028d24
Amos Ko <amosk@d142-058-218-013.wireless.sfu.ca>
2021-11-27 16:56:25 -0800

    remove unused js

--- website/static/home.js
+++ website/static/home.js
@@ -5,16 +5,3 @@ function handleGive() {
 function handleGet() {
     // alert("get!")
 }
-
-var listings = document.getElementsByClassName("listingColumn");
-var i;
-
-function two() {
-    for (i = 0; i < listings.length; i++) {
-        console.log(listings.length)
-        // listings[i].style.msFlex = "50%";  // IE10
-        // listings[i].style.flex = "50%";
-    }
-  }
-
-two();
